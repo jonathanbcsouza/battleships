f75c903fe54089248ed1b8604ddb7e5cf8c67557
Jonathan Souza <jonathanbcsouza@gmail.com>
2024-01-06 23:38:41 +1300

    refactor: abstract functions into a new helper module

---
+++ helpers/helpers.js
@@ -0,0 +1,152 @@
+const HIT_DIST = 0;
+const HOT_DIST = 1;
+const WARM_DIST = 3;
+const COLD_DIST = 5;
+
+// Grid management
+export function buildGrid(size) {
+  let columns = [];
+
+  for (let i = 0; i < size; i++) {
+    let rows = [];
+    for (let j = 0; j < size; j++) {
+      rows.push(0);
+    }
+    columns.push(rows);
+  }
+
+  return columns;
+}
+
+export function placeShips(grid, gridSize, numShips) {
+  let shipsPlaced = 0;
+
+  while (shipsPlaced < numShips) {
+    const randomRow = Math.floor(Math.random() * gridSize);
+    const randomCol = Math.floor(Math.random() * gridSize);
+
+    if (grid[randomRow][randomCol] == 0) {
+      grid[randomRow][randomCol] = 1;
+      shipsPlaced++;
+    }
+  }
+}
+
+export function removeShip(grid, x, y) {
+  console.log('removing ship', x, y);
+  grid[x][y] = null;
+  console.table(grid);
+  return grid;
+}
+
+export function revealGrid(grid) {
+  const table = document.createElement('table');
+
+  for (let i = 0; i < grid.length; i++) {
+    const row = document.createElement('tr');
+    for (let j = 0; j < grid[i].length; j++) {
+      const cell = document.createElement('td');
+      cell.textContent = grid[i][j];
+      row.appendChild(cell);
+    }
+    table.appendChild(row);
+  }
+  board.appendChild(table);
+}
+
+// Game Logic
+export function locateShips(grid, gridSize) {
+  let shipsLocation = [];
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      if (grid[i][j] === 1) {
+        shipsLocation.push([i, j]);
+      }
+    }
+  }
+
+  return shipsLocation;
+}
+
+export function getClosestShipDistance(shiftedX, shiftedY, shipsCoordinates) {
+  const distances = [];
+
+  for (let i = 0; i < shipsCoordinates.length; i++) {
+    const distFromShip =
+      Math.abs(shiftedX - shipsCoordinates[i][0]) +
+      Math.abs(shiftedY - shipsCoordinates[i][1]);
+
+    distances.push(distFromShip);
+  }
+
+  return Math.min(...distances);
+}
+
+export function useRockets(rocketsCount) {
+  rocketsCount = rocketsCount - 1;
+  return rocketsCount;
+}
+
+export function setGameState(rocketsCount, shipsCount, gameState) {
+  if (rocketsCount === 0) {
+    gameState = 'lose';
+  }
+
+  if (shipsCount === 0) {
+    gameState = 'win';
+  }
+
+  if (rocketsCount > 0 && shipsCount > 0) {
+    gameState = 'playing';
+  }
+
+  return gameState;
+}
+
+export function resetGame(
+  welcomeMsg,
+  welcomeMsgElement,
+  numRockets,
+  rockets,
+  shipsDestroyed,
+  startBtn,
+  restartBtn,
+  board,
+  gameState
+) {
+  while (board.firstChild) {
+    board.removeChild(board.firstChild);
+  }
+  welcomeMsgElement.innerHTML = welcomeMsg;
+  rockets.innerHTML = numRockets;
+  shipsDestroyed.innerHTML = 0;
+
+  startBtn.style.display = 'block';
+  restartBtn.style.display = 'none';
+
+  gameState = 'init';
+}
+
+// Ui Updates
+export function launchRocket(x, y) {
+  alert(`Rocket launched to ${x + 1}, ${y + 1}!`);
+}
+
+export function radarFeedback(d) {
+  if (d === HIT_DIST) {
+    alert('HIT!');
+    alert('BOOM!');
+    return true;
+  } else if (d >= COLD_DIST) {
+    alert('COLD');
+  } else if (d >= WARM_DIST) {
+    alert('WARM');
+  } else {
+    alert('HOT');
+  }
+  return false;
+}
+
+export function updateScreen(element, value) {
+  element.innerHTML = value;
+}
--- index.js
+++ index.js
@@ -1,37 +1,65 @@
-const HIT_DIST = 0;
-const HOT_DIST = 1;
-const WARM_DIST = 3;
-const COLD_DIST = 5;
+import {
+  buildGrid,
+  getClosestShipDistance,
+  launchRocket,
+  locateShips,
+  placeShips,
+  radarFeedback,
+  removeShip,
+  resetGame,
+  revealGrid,
+  setGameState,
+  updateScreen,
+  useRockets,
+} from './helpers/helpers.js';
 
 const gridSize = 4;
 const numRockets = 2;
 const numShips = 2;
-
+const msgGameOver = `Game Over! You Lose! 😭`;
+const msgYouWin = `Congratulations, You Win! 🎉`;
 const welcomeMsg =
   `Welcome aboard. You are in the middle of a war and your mission is to destroy the remaining ${numShips} opponent ships! You have ${numRockets} rockets left. You can use the rockets to attack the ships. ` +
   `<br>` +
   `Good luck!`;
 
+const trophies = document.getElementById('trophies');
 const board = document.getElementById('board');
 const startBtn = document.getElementById('startButton');
-startBtn.addEventListener('click', playGame);
 const restartBtn = document.getElementById('restart');
-restartBtn.addEventListener('click', resetGame);
-restartBtn.style.display = 'none';
+
+let grid = [];
+let shipsCount = numShips;
+let rocketsCount = numRockets;
+let shipsDestroyedCount = 0;
+let trophiesCount = 0;
+let gameState = 'init';
+let closestShipDist = gridSize;
 
 let welcomeMsgElement = document.getElementById('welcome_msg');
 let rockets = document.getElementById('rockets');
-let ships_destroyed = document.getElementById('ships_destroyed');
-welcomeMsgElement.innerHTML = welcomeMsg;
-rockets.innerHTML = numRockets;
-ships_destroyed.innerHTML = 0;
+let shipsDestroyed = document.getElementById('shipsDestroyed');
 
-let shipsCount = numShips;
-let rocketsCount = numRockets;
-let trophiesCount = 0;
-let gameState = 'init';
-const trophies = document.getElementById('trophies');
-trophies.innerHTML = trophiesCount;
+startBtn.addEventListener('click', playGame);
+restartBtn.style.display = 'none';
+restartBtn.addEventListener('click', function () {
+  resetGame(
+    welcomeMsg,
+    welcomeMsgElement,
+    numRockets,
+    rockets,
+    shipsDestroyed,
+    startBtn,
+    restartBtn,
+    board,
+    gameState
+  );
+});
+
+updateScreen(welcomeMsgElement, welcomeMsg);
+updateScreen(rockets, numRockets);
+updateScreen(shipsDestroyed, 0);
+updateScreen(trophies, trophiesCount);
 
 function playGame() {
   startBtn.style.display = 'none';
@@ -39,8 +67,9 @@ function playGame() {
   if (gameState === 'init') {
     shipsCount = numShips;
     rocketsCount = numRockets;
+    shipsDestroyedCount = 0;
     grid = buildGrid(gridSize);
-    placeShips(numShips);
+    placeShips(grid, gridSize, numShips);
     console.table(grid);
     gameState = 'playing';
   }
@@ -53,180 +82,63 @@ function playGame() {
     const shiftedX = coordinateX - 1;
     const shiftedY = coordinateY - 1;
 
-    const shipsCoordinates = locateShips();
+    const shipsCoordinates = locateShips(grid, gridSize);
 
-    closestShipDist = getClosestclosestShipDistance(
+    closestShipDist = getClosestShipDistance(
       shiftedX,
       shiftedY,
       shipsCoordinates
     );
 
-    launchRocket(shiftedX, shiftedY, closestShipDist);
+    rocketsCount = useRockets(rocketsCount);
 
-    const gameStatus = setGameStatus();
+    updateScreen(rockets, rocketsCount);
 
-    restartBtn.style.display = 'block';
+    launchRocket(shiftedX, shiftedY);
 
-    switch (gameStatus) {
+    const isShipDestroyed = radarFeedback(closestShipDist);
+
+    if (isShipDestroyed) {
+      grid = removeShip(grid, shiftedX, shiftedY);
+      shipsCount = shipsCount - 1;
+      shipsDestroyedCount = shipsDestroyedCount + 1;
+    }
+
+    updateScreen(shipsDestroyed, shipsDestroyedCount);
+
+    const status = setGameState(rocketsCount, shipsCount, gameState);
+
+    switch (status) {
       case 'lose':
-        welcomeMsgElement.innerHTML = `Game Over! You Lose! 😭`;
-        restartBtn.innerHTML = 'Try Again';
         shipsCount = numShips;
         rocketsCount = numRockets;
-        revealGrid();
+        gameState = 'init';
+        updateScreen(welcomeMsgElement, msgGameOver);
+        updateScreen(restartBtn, 'Try Again');
+        restartBtn.style.display = 'block';
+        revealGrid(grid);
         break;
       case 'win':
-        welcomeMsgElement.innerHTML = `Congratulations, You Win! 🎉`;
-        trophiesCount = trophiesCount + 1;
-        gameState = 'init';
         shipsCount = numShips;
         rocketsCount = numRockets;
-        trophies.innerHTML = trophiesCount;
-        startButton.innerHTML = 'Start a New Game';
-        revealGrid();
+        gameState = 'init';
+        trophiesCount = trophiesCount + 1;
+        updateScreen(welcomeMsgElement, msgYouWin);
+        updateScreen(restartBtn, 'Start a New Game');
+        updateScreen(trophies, trophiesCount);
+        restartBtn.style.display = 'block';
+        revealGrid(grid);
         break;
       default:
-        welcomeMsgElement.innerHTML =
+        updateScreen(
+          welcomeMsgElement,
           `🎙️ Roger!` +
-          `<br>` +
-          `You have ${rocketsCount} rockets left and ${shipsCount} opponent ships remaining. Keep it Up!`;
-        startBtn.style.display = 'block';
+            `<br>` +
+            `You have ${rocketsCount} rockets left and ${shipsCount} opponent ships remaining. Keep it Up!`
+        );
         restartBtn.style.display = 'none';
-        startButton.innerHTML = 'Continue Game ➡️';
+        startBtn.style.display = 'block';
+        updateScreen(startButton, 'Continue Game ➡️');
     }
   }
-}
-
-function revealGrid() {
-  const table = document.createElement('table');
-  for (let i = 0; i < grid.length; i++) {
-    const row = document.createElement('tr');
-    for (let j = 0; j < grid[i].length; j++) {
-      const cell = document.createElement('td');
-      cell.textContent = grid[i][j];
-      row.appendChild(cell);
-    }
-    table.appendChild(row);
-  }
-  board.appendChild(table);
-}
-
-function getClosestclosestShipDistance(shiftedX, shiftedY, shipsCoordinates) {
-  const distances = [];
-
-  for (let i = 0; i < shipsCoordinates.length; i++) {
-    const distFromShip =
-      Math.abs(shiftedX - shipsCoordinates[i][0]) +
-      Math.abs(shiftedY - shipsCoordinates[i][1]);
-
-    distances.push(distFromShip);
-  }
-
-  return Math.min(...distances);
-}
-
-function placeShips(numShips) {
-  let shipsPlaced = 0;
-
-  while (shipsPlaced < numShips) {
-    const randomRow = Math.floor(Math.random() * gridSize);
-    const randomCol = Math.floor(Math.random() * gridSize);
-
-    if (grid[randomRow][randomCol] == 0) {
-      grid[randomRow][randomCol] = 1;
-      shipsPlaced++;
-    }
-  }
-}
-
-function locateShips() {
-  let shipsLocation = [];
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-      if (grid[i][j] === 1) {
-        shipsLocation.push([i, j]);
-      }
-    }
-  }
-
-  return shipsLocation;
-}
-
-function launchRocket(x, y, d) {
-  useRockets();
-  rockets.innerHTML = rocketsCount;
-  alert(`Rocket launched to ${x + 1}, ${y + 1}!`);
-  radarFeedback(x, y, d);
-}
-
-function radarFeedback(x, y, d) {
-  if (d === HIT_DIST) {
-    alert('HIT!');
-    hit(x, y);
-  } else if (d >= COLD_DIST) {
-    alert('COLD');
-  } else if (d >= WARM_DIST) {
-    alert('WARM');
-  } else {
-    alert('HOT');
-  }
-}
-
-function useRockets() {
-  rocketsCount = rocketsCount - 1;
-}
-
-function hit(x, y) {
-  removeShip(x, y);
-  shipsCount = shipsCount - 1;
-  ships_destroyed.innerHTML = numShips - shipsCount;
-  alert('BOOM!');
-}
-
-function removeShip(x, y) {
-  grid[x][y] = 'x';
-}
-
-function setGameStatus() {
-  if (rocketsCount === 0) {
-    gameState = 'lose';
-  }
-
-  if (shipsCount === 0) {
-    gameState = 'win';
-  }
-
-  if (rocketsCount > 0 && shipsCount > 0) {
-    gameState = 'playing';
-  }
-
-  return gameState;
-}
-
-function buildGrid(size) {
-  let columns = [];
-
-  for (let i = 0; i < size; i++) {
-    let rows = [];
-    for (let j = 0; j < size; j++) {
-      rows.push(0);
-    }
-    columns.push(rows);
-  }
-
-  return columns;
-}
-
-function resetGame() {
-  while (board.firstChild) {
-    board.removeChild(board.firstChild);
-  }
-  welcomeMsgElement.innerHTML = welcomeMsg;
-  rockets.innerHTML = numRockets;
-  ships_destroyed.innerHTML = 0;
-
-  startBtn.style.display = 'block';
-  restartBtn.style.display = 'none';
-
-  gameState = 'init';
 }
