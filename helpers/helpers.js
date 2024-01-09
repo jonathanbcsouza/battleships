1eca828d8e83231fbd7f078cca549f57817af51e
Jonathan Souza <jonathanbcsouza@gmail.com>
2024-01-09 22:46:31 +1300

    fix: force location.reload() only after the fetch

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -186,10 +186,11 @@ export function resetScore(username) {
     }),
   })
     .then((response) => response.text())
-    .then((data) => console.log(data))
+    .then((data) => {
+      console.log(data);
+      location.reload();
+    })
     .catch((error) => {
       console.error('Error to reset data:', error);
     });
-
-  location.reload();
 }
