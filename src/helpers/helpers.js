f790b39880271f4d376fb857674fb777adfe60ca
Jonathan Souza <jonathanbcsouza@gmail.com>
2024-01-15 13:52:49 +1300

    refactor: abstract ajax functions

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,33 +1,5 @@
 import * as config from '../configs/constants.js';
 
-// Grid management
-export async function getGrid() {
-  try {
-    const response = await fetch('../helpers/createGrid.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
-        size: config.GRID_SIZE,
-        numShips: config.NUM_SHIPS,
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const grid = await response.json();
-
-    return grid;
-  } catch (error) {
-    console.log(
-      'There was a problem with the fetch operation: ' + error.message
-    );
-  }
-}
-
 export function removeShip(grid, x, y) {
   grid[x][y] = config.EXPLOSION_ICON;
   return grid;
@@ -147,42 +119,4 @@ export function getValidCoordinates(coordinate, maxNumber) {
   );
 
   return coordinateValue;
-}
-
-// DB Operations
-function handleScore(username, action) {
-  alert(action === 'add' ? 'Trophy Earned!' : 'Score Reset!');
-  fetch('../../http_requests.php', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams({
-      username: username,
-      action: action,
-    }),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.text();
-    })
-    .then((data) => {
-      console.log(data);
-      if (action === 'reset') {
-        location.reload();
-      }
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-}
-
-export function addScore(username) {
-  handleScore(username, 'add');
-}
-
-export function resetScore(username) {
-  handleScore(username, 'reset');
 }
