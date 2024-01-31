686e0d3bf9e1c0f3c769a5276fbe667b52ef3897
Jonathan Souza <jonathanbcsouza@gmail.com>
2024-02-01 12:35:44 +1300

    feat: add optional config file if using AWS parameter store instead of .env

---
+++ aws_connection.php
@@ -0,0 +1,39 @@
+<?php
+require 'vendor/autoload.php';
+
+use App\Classes\Database;
+use Aws\Ssm\SsmClient;
+
+// Retrieving credentials from AWS SSM / Parameter Store
+$client = new SsmClient([
+  'version' => 'latest',
+  'region'  => 'ap-southeast-2' 
+]);
+
+$parameters = $client->getParameters([
+  'Names' => ['SERVER', 'USERNAME', 'PASSWORD', 'DATABASE'],
+  'WithDecryption' => true
+])->toArray();
+
+$params = [];
+
+foreach ($parameters['Parameters'] as $param) {
+  $params[$param['Name']] = $param['Value'];
+}
+
+$server_name = $params['SERVER'];
+$db_username = $params['USERNAME'];
+$password = $params['PASSWORD'];
+$db_name = $params['DATABASE'];
+
+$conn = new mysqli($server_name, $db_username, $password);
+
+if ($conn->connect_error) {
+  throw new Exception("Connection failed: " . $conn->connect_error);
+}
+
+// Bootstrapping a new DB and table
+$database = new Database($conn);
+$database->createDatabase($db_name);
+$database->createUsersTable();
+$database->createUsersConfigTable();
